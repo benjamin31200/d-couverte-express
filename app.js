e9ff5c0aae4edba1fc32666074f19c660dad16fe
benjamin31200 <benjamindeneux@hotmail.com>
2022-07-20 17:58:42 +0200

    déouverte express

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+
+const port = 5000;
+
+app.get("/", (req, res) => {
+  res.send("Welcome to Express");
+});
+
+app.listen(port, (err) => {
+  if (err) {
+    console.error('Something bad happened');
+  } else {
+    console.log(`server is listening on ${port}`);
+  }
+});
