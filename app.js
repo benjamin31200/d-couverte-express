5919140f5e9138e5031ae6ab72b1b2de92a7bfd0
benjamin31200 <benjamindeneux@hotmail.com>
2022-07-20 19:02:51 +0200

    routing avec express

--- app.js
+++ app.js
@@ -3,9 +3,17 @@ const app = express();
 
 const port = 5000;
 
-app.get("/", (req, res) => {
+const welcome = (req, res) => {
   res.send("Welcome to Express");
-});
+};
+
+app.get("/", welcome);
+
+const welcomeName = (req, res) => {
+  res.send(`Welcome ${req.params.name}`);
+};
+
+app.get("/users/:name", welcomeName);
 
 app.listen(port, (err) => {
   if (err) {
